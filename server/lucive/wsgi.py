1167e9f338bea8f9afb3f8504f7295c57fab057c
anujdhillxn <anuj.dhillon.14@gmail.com>
2024-11-06 22:24:02 +0530

    reverted

--- server/lucive/wsgi.py
+++ server/lucive/wsgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import sys
-from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lucive.settings')
-execute_from_command_line(['manage.py', 'makemigrations'])
-execute_from_command_line(['manage.py', 'migrate'])
-sys.exit()
+
 application = get_wsgi_application()
